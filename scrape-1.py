3c8cc02562120905b4eca1aade730a089e3645cd
riteshdavid <101193193+riteshdavid@users.noreply.github.com>
2022-10-16 08:47:15 +0530

    Adding Interactivity of Program

--- scrape-1.py
+++ scrape-1.py
@@ -2,15 +2,19 @@
 
 from bs4 import BeautifulSoup
 import requests
+import time
 
-
+print("[]INFO: Getting to python.org/jobs ...")
 html_text=requests.get('https://www.python.org/jobs/')
+time.sleep(2)
 
 
 
 soup=BeautifulSoup(html_text.text,'html.parser')
 tags=soup.find('ol',class_='list-recent-jobs list-row-container menu')
 company_name=tags.find_all('li')
+print("[]INFO: Analyzing Page For Jobs ..." )
+time.sleep(2)
 for company in company_name:
     companyname=company.find('span',class_="listing-company-name")
     company_loc=company.find('span',class_="listing-location")
